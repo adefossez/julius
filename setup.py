d1ee9e225244d83822cd7d3b81422ea9b2e86a13
Alexandre Defossez <alexandre.defossez@gmail.com>
2020-11-03 12:02:27 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ URL = 'https://github.com/adefossez/julius'
 EMAIL = 'alexandre.defossez@gmail.com'
 AUTHOR = 'Alexandre Défossez'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 HERE = Path(__file__).parent
 
