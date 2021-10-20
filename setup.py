2a43ba41b1f5f2914e4dc6ba6d94da63657e701a
Alexandre Defossez <alexandre.defossez@gmail.com>
2021-10-20 10:28:45 +0200

    adding py.typed

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ URL = 'https://github.com/adefossez/julius'
 EMAIL = 'alexandre.defossez@gmail.com'
 AUTHOR = 'Alexandre Défossez'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.2.5"
+VERSION = "0.2.6"
 
 HERE = Path(__file__).parent
 
